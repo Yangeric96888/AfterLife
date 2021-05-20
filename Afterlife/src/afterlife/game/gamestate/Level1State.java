a3aeeaa68d8c1b253091119bac5bdb84cf9a3cd1
Yangeric96888 <Eric1eric2eric3eric4eric5@outlook.com>
2021-05-20 15:00:20 -0700

    05/20 Sorted files into packages

---
+++ Afterlife/src/afterlife/game/gamestate/Level1State.java
@@ -0,0 +1,41 @@
+package afterlife.game.gamestate;
+import java.awt.Graphics;
+
+public class Level1State extends GameState{
+
+	public Level1State(GameStateManager gsm) {
+		super(gsm);
+		
+	}
+
+	@Override
+	public void init() {
+		
+		
+	}
+
+	@Override
+	public void tick() {
+		
+		
+	}
+
+	@Override
+	public void draw(Graphics g) {
+		
+		
+	}
+
+	@Override
+	public void keyPressed(int k) {
+		
+		
+	}
+
+	@Override
+	public void keyReleased(int k) {
+		
+		
+	}
+
+}

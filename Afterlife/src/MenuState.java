05c21e1be19f2d2ff739f4d9de479cf041f160b9
Yangeric96888 <Eric1eric2eric3eric4eric5@outlook.com>
2021-05-20 14:16:30 -0700

    05/20 Finished MenuState
    - Added code to move between buttons

--- Afterlife/src/MenuState.java
+++ Afterlife/src/MenuState.java
@@ -1,5 +1,7 @@
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.event.KeyEvent;
 
 public class MenuState extends GameState {
 	
@@ -25,27 +27,55 @@ public class MenuState extends GameState {
 
 	@Override
 	public void draw(Graphics g) {
+		
+		// Set background
+		g.setColor(new Color(50, 70, 200));
+		g.fillRect(0, 0, GamePanel.WIDTH, GamePanel.HEIGHT);
+		
+		// Draws the buttons
 		for (int i = 0; i < options.length; i++) {
 			if (i == currentSelection) {
 				g.setColor(Color.green);
 			} else {
-				g.setColor(Color.black);
+				g.setColor(Color.white);
 			}
 			
-			g.drawString(options[i], GamePanel.WIDTH / 2 - 50, 50 + i * 30);
+			// g.drawLine(GamePanel.WIDTH / 2, 0, GamePanel.WIDTH / 2, GamePanel.HEIGHT);
+			g.setFont(new Font("Arial", Font.PLAIN, 72));
+			g.drawString(options[i], GamePanel.WIDTH / 2 - 75, 150 + i * 150);
 		}
 		
 	}
 
 	@Override
 	public void keyPressed(int k) {
-		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void keyReleased(int k) {
-		// TODO Auto-generated method stub
+		if (k == KeyEvent.VK_DOWN) {
+			currentSelection++;
+			if (currentSelection >= options.length) {
+				currentSelection = 0;
+			}
+		} else if (k == KeyEvent.VK_UP) {
+			currentSelection--;
+			if (currentSelection < 0) {
+				currentSelection = options.length - 1;
+			}
+		}
+		
+		// When players picks a button
+		if (k == KeyEvent.VK_ENTER) {
+			if (currentSelection == 0) {
+				
+			} else if (currentSelection == 1) {
+				
+			} else if (currentSelection == 2) {
+				System.exit(0);
+			}
+		}
 		
 	}
 

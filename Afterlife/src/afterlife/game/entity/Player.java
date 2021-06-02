71f777dd7bd18119dfbceba954261032f39e36d2
Toby Sagi <tt8827@cerian.com>
2021-06-02 14:52:31 -0700

    06/02 Created Map Reader
    - Used a BufferedReader and InputStream to build maps from a file\
    - Some issues with collision
    - Some issues with movement

--- Afterlife/src/afterlife/game/entity/Player.java
+++ Afterlife/src/afterlife/game/entity/Player.java
@@ -15,136 +15,141 @@ import afterlife.game.object.Block;
 import afterlife.game.physic.Collision;
 
 public class Player extends Rectangle {
-	
-	//Movement booleans
+
+	// Movement booleans
 	private boolean right = false, left = false, jumping = false, falling = false;
 	private boolean topCollision = false;
-	
+
 	// Location / size
 	private double x, y;
 	private int width, height;
-	
+
 	// Move speed
 	private double moveSpeed = 5;
-	
+
 	// Jump speed
 	private double jumpSpeed = 0.025;
 	private double currentJumpSpeed = jumpSpeed;
-	
+
 	// Fall speed
-	private double maxFallSpeed  = 0.1;
+	private double maxFallSpeed = 0.15;
 	private double currentFallSpeed = 0.001;
 	private double acceleration = 0.000001;
-	
+
 	public Player(int width, int height) {
 		x = GamePanel.WIDTH / 2;
 		y = GamePanel.HEIGHT / 2;
 		this.width = width;
 		this.height = height;
 	}
-	// test commit and push
-	
+
 	public void tick(Block[][] b) {
-		
-		// Collision 
+
+		// Collision
 		for (Block[] currentBlockRow : b) {
-			
-			for (Block currentB : currentBlockRow ) {
-				// Right
-				if (Collision.playerBlock(new Point((int) x + width + (int) GameState.xOffset + 2, (int) y + (int) GameState.yOffset + 2), currentB) || 
-					Collision.playerBlock(new Point((int) x + width + (int) GameState.xOffset + 2, (int) y + height + (int) GameState.yOffset - 1), currentB)) {
+
+			for (Block currentB : currentBlockRow) {
+				if (currentB.getID() != 0) {
+					// Right
+					if (Collision.playerBlock(new Point((int) x + width + (int) GameState.xOffset + 2, (int) y + (int) GameState.yOffset + 2), currentB)
+							|| Collision.playerBlock(new Point((int) x + width + (int) GameState.xOffset + 2, (int) y + height + (int) GameState.yOffset - 1), currentB)) {
 						right = false;
 						GameState.xOffset--;
 					}
-				
-				// Left
-				if (Collision.playerBlock(new Point((int) x + (int) GameState.xOffset - 1, (int) y + (int) GameState.yOffset + 2), currentB) || 
-					Collision.playerBlock(new Point((int) x + (int) GameState.xOffset - 1, (int) y + height + (int) GameState.yOffset - 1), currentB)) {
+
+					// Left
+					if (Collision.playerBlock(new Point((int) x + (int) GameState.xOffset - 1, (int) y + (int) GameState.yOffset + 2), currentB)
+							|| Collision.playerBlock(new Point((int) x + (int) GameState.xOffset - 1, (int) y + height + (int) GameState.yOffset - 1), currentB)) {
 						left = false;
 						GameState.xOffset++;
-				}
-				
-				// Top
-				if (Collision.playerBlock(new Point((int) x + (int) GameState.xOffset + 1, (int) y + (int) GameState.yOffset), currentB) || 
-					Collision.playerBlock(new Point((int) x + width + (int) GameState.xOffset - 1, (int) y + (int) GameState.yOffset), currentB)) {
+					}
+
+					// Top
+					if (Collision.playerBlock(new Point((int) x + (int) GameState.xOffset + 1, (int) y + (int) GameState.yOffset), currentB)
+							|| Collision.playerBlock(new Point((int) x + width + (int) GameState.xOffset - 2, (int) y + (int) GameState.yOffset), currentB)) {
 						y = currentB.getY() - (int) GameState.yOffset;
 						jumping = false;
 						falling = true;
-				}
-				
-				// Bottom
-				
-				if (Collision.playerBlock(new Point((int) x + (int) GameState.xOffset + 2, (int) y + height + (int) GameState.yOffset + 1), currentB) || 
-					Collision.playerBlock(new Point((int) x + width + (int) GameState.xOffset - 1, (int) y + height + (int) GameState.yOffset + 1), currentB)) {
+					}
+
+					// Bottom
+					if (Collision.playerBlock(new Point((int) x + (int) GameState.xOffset + 2, (int) y + height + (int) GameState.yOffset + 1), currentB)
+							|| Collision.playerBlock(new Point((int) x + width + (int) GameState.xOffset - 2, (int) y + height + (int) GameState.yOffset + 1), currentB)) {
 						falling = false;
 						topCollision = true;
 						System.out.print("b");
-				} else if (!topCollision && !jumping) {
+					} else if (!topCollision && !jumping) {
 						falling = true;
 						System.out.print("c");
-				} 
+					}
+				}
 			}
-		} 
-		
+		}
+
 		topCollision = false;
-		
+
 		// Movement for player
-		// !!! The movement right now is bugged because when you press, it just flies off
+		// !!! The movement right now is bugged because when you press, it just flies
+		// off
 		if (right) {
 			GameState.xOffset += moveSpeed;
 			right = false;
-		} 
-		
+		}
+
 		if (left) {
 			GameState.xOffset -= moveSpeed;
 			left = false;
 		}
-		
-		// Jumping 
+
+		// Jumping
 		if (jumping) {
 			System.out.print("b");
 			GameState.yOffset -= currentJumpSpeed;
 			currentJumpSpeed -= acceleration;
-			
+
 			System.out.print("b");
-			
+
 			if (currentJumpSpeed <= 0) {
 				currentJumpSpeed = jumpSpeed;
 				jumping = false;
 				falling = true;
 			}
 		}
-		
+
 		if (falling) {
 			GameState.yOffset += currentFallSpeed;
-			
+
 			if (currentFallSpeed < maxFallSpeed) {
 				currentFallSpeed += acceleration;
 			}
 		}
-		
+
 		if (!falling) {
 			currentFallSpeed = 0.001;
 		}
 	}
-	
-	public void draw (Graphics g) {
+
+	public void draw(Graphics g) {
 		g.setColor(Color.BLACK);
 		g.fillRect((int) x, (int) y, width, height);
 	}
-	
+
 	public void keyPressed(int k) {
-		if (k == KeyEvent.VK_D) right = true;
-		if (k == KeyEvent.VK_A) left = true;
-		if (k == KeyEvent.VK_SPACE && !jumping && !falling) jumping = true;	// To add double-jumping, removing !jumping and !falling
+		if (k == KeyEvent.VK_D)
+			right = true;
+		if (k == KeyEvent.VK_A)
+			left = true;
+		if (k == KeyEvent.VK_SPACE && !jumping && !falling)
+			jumping = true; // To add double-jumping, removing !jumping and !falling
 	}
-	
+
 	public void keyReleased(int k) {
 		if (k == KeyEvent.VK_D) {
 			right = false;
 		}
-		if (k == KeyEvent.VK_A) left = false;	
+		if (k == KeyEvent.VK_A)
+			left = false;
 
 	}
-	
+
 }

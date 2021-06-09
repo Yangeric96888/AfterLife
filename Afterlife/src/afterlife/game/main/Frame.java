a19de4522a8eaa18725e4f1de087e6c873dcbea7
Yangeric96888 <Eric1eric2eric3eric4eric5@outlook.com>
2021-06-09 16:14:27 -0700

    06/09 Commit

---
+++ Afterlife/src/afterlife/game/main/Frame.java
@@ -0,0 +1,53 @@
+package afterlife.game.main;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+public class Frame extends JPanel {
+	
+	/*
+	 * Create Objects
+	 */ 
+	
+	/*
+	 * Create music 
+	 */
+//	Music example = new Music("sound/exampleName.wav", false);
+
+	
+	public void paint(Graphics g) {
+		super.paint(g);	// Refresh the JFrame 
+	}
+	
+	public static void main(String[] arg) {
+		Frame f = new Frame();
+	}
+	
+	public Frame() {
+		
+		JFrame f = new JFrame("Afterlife");
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setResizable(false);
+		f.setLayout(new BorderLayout());
+		f.add(new GamePanel(), BorderLayout.CENTER);
+		f.pack();
+		f.setLocationRelativeTo(null);
+		f.setVisible(true);
+		
+		f.setBackground(Color.blue);
+		f.add(this);
+		
+	}
+	
+}
---
+++ Afterlife/src/afterlife/game/resources/Images.java
@@ -0,0 +1,25 @@
+package afterlife.game.resources;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class Images {
+	
+	public static BufferedImage[] blocks;
+	
+	public Images() {
+		blocks = new BufferedImage[1];
+		try {
+		blocks[0] = ImageIO.read(getClass().getResourceAsStream("/Blocks/block_brick.png"));
+//		blocks[0] = ImageIO.read(getClass().getResourceAsStream("/Blocks/cobblestone_mossy.png"));
+//		blocks[0] = ImageIO.read(getClass().getResourceAsStream("/Blocks/dark_prismarine.png"));
+//		blocks[0] = ImageIO.read(getClass().getResourceAsStream("/Blocks/farmland_moist.png"));
+//		blocks[0] = ImageIO.read(getClass().getResourceAsStream("/Blocks/glass_magenta.png"));
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}

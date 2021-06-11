140edf7201583a4386eec1a389c5dc1645a5e716
Toby Sagi <tt8827@cerian.com>
2021-06-11 12:15:28 -0700

    06/11 Changed Block Color
    - Blocks are now BLUE

--- Afterlife/src/afterlife/game/resources/Images.java
+++ Afterlife/src/afterlife/game/resources/Images.java
@@ -15,11 +15,12 @@ public class Images {
 	public Images() {
 		blocks = new BufferedImage[1];
 		try {
-		blocks[0] = ImageIO.read(getClass().getResourceAsStream("/Blocks/block_brick.png"));
+//		blocks[0] = ImageIO.read(getClass().getResourceAsStream("/Blocks/block_brick.png"));
 //		blocks[0] = ImageIO.read(getClass().getResourceAsStream("/Blocks/cobblestone_mossy.png"));
 //		blocks[0] = ImageIO.read(getClass().getResourceAsStream("/Blocks/dark_prismarine.png"));
 //		blocks[0] = ImageIO.read(getClass().getResourceAsStream("/Blocks/farmland_moist.png"));
 //		blocks[0] = ImageIO.read(getClass().getResourceAsStream("/Blocks/glass_magenta.png"));
+		blocks[0] = ImageIO.read(getClass().getResourceAsStream("/Blocks/Blue_block.png"));
 		} catch(IOException e) {
 			e.printStackTrace();
 		}

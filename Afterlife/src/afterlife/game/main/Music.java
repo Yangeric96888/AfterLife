cf218f608671bdd6bd67e26cbf86b4ef4be46802
Toby Sagi <tt8827@cerian.com>
2021-06-11 11:31:05 -0700

    06/11 Maybe Final Map

--- Afterlife/src/afterlife/game/main/Music.java
+++ Afterlife/src/afterlife/game/main/Music.java
@@ -7,6 +7,7 @@ import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.sound.sampled.DataLine;
+import javax.sound.sampled.FloatControl;
 import javax.sound.sampled.LineEvent;
 import javax.sound.sampled.LineListener;
 import javax.sound.sampled.UnsupportedAudioFileException;
@@ -23,6 +24,7 @@ import javax.sound.sampled.UnsupportedAudioFileException;
  */
 
 public class Music  implements Runnable  {
+	
 	Thread t;
 	File audioFile ;
     AudioInputStream audioStream;
